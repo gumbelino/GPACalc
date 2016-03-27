06ccc93d5047af04f5ad8e5dd62426fd43dbba7f
Gustavo Umbelino <gumbelin@ucsd.edu>
2016-03-26 21:31:59 -0700

    Clean up files

--- GUI.py
+++
@@ -1,5 +0,0 @@
-from Tkinter import *
-
-win = Tk()
-b1 = Button(win,text="One")
-b2 = Button(win,text="Two")
--- URL.py
+++
@@ -1,5 +0,0 @@
-import urllib.request
-
-local_filename, headers = urllib.request.urlretrieve('http://python.org/')
-html = open('page.txt', 'w')
-html.close()
